b2415a575ed2bf58cea2beed50684a0ef23ff588
SantiagoGV2006 <tyago.2006gv@gmail.com>
2025-05-03 08:00:59 -0500

    arreglos

--- Entity/DTOs/UserDTO.cs
+++ Entity/DTOs/UserDTO.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set;}
+        public required string Email { get; set;}
     }
 }
